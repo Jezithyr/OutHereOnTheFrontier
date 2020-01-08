e20e1f5ff91865a5b3aa1d734d8da8ec5a4b0783
Jesse <jmaster9999@gmail.com>
2020-01-08 14:38:15 -0800

    Added construction subsystem

---
+++ Assets/Scripts/Core/ConstructionSystem/Building.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : ScriptableObject
+{
+    [SerializeField] private GameObject prefab;
+    public GameObject Prefab{get => prefab;}
+    
+    private ConstructionManager constructionManager; 
+
+}
---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : ScriptableObject
+{
+    [SerializeField] private ConstructionManager ConstructionController ;
+    public ConstructionManager ConstructionSubSystem{get => ConstructionController;}
+
+    [SerializeField] private JobManager JobController;
+    public JobManager JobSubSystem{get => JobController;}
+
+    [SerializeField] private PawnManager pawnManager;
+    public PawnManager PawnSubSystem{get => pawnManager;}
+
+    [SerializeField] private GridSystem ActiveGrid;
+    public GridSystem Grid{get => ActiveGrid;}
+}
