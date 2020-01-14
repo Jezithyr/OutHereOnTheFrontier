d410eec2ee4f2164bc14c166e821bec29dd77a80
Jesse Rougeau <Jmaster9999@gmail.com>
2020-01-14 11:34:44 -0800

    Added event system framework
    -created placeholder demo UI

--- Assets/Scripts/Systems/EventSystem/EventModule.cs
+++ Assets/Scripts/Systems/EventSystem/EventModule.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "GameFramework/SubSystems/EventModule")]
 public class EventModule : Module
-{
+{   
+    [SerializeField] private List<Event> activeEvents = new List<Event>() ;
+
+
+
+
+
     public override void Initialize()
     {
         
