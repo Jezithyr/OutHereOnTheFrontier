07a5751cdffa870da491cbe065eb448be34fff95
Evan Landry <Snoopy20015@gmail.com>
2020-01-14 19:27:19 -0800

    -Added more NavMesh Test code.
    I gave it a shot but this is what I was able to come up with so far. Groundwork is there, will try to continue at home. Thanks Jesse!

---
+++ Assets/Scripts/Testing/NavMesh Test/RandomPathing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+// This script will be testing the functions of random AI pathing along
+// a sample scene in the Engine.
+public class RandomPathing : MonoBehaviour
+{
+    // SerializedFields will be kept up here
+    [SerializeField] public float newPathTimer;
+
+
+
+    NavMeshAgent newMeshAgent;
+
+    private void Start()
+    {
+        newMeshAgent =  GetComponent<NavMeshAgent>();
+    }
+
+    Vector3 getNewRandomPos()
+    {
+        // Randomize a pair of X and Z coords for the AI. CAN BE SERIALIZED.
+        float x = Random.Range(-20, 20);
+        float z = Random.Range(-20, 20);
+
+        Vector3 pos = new Vector3(x, 0, z);
+        return pos;
+    }
+}
