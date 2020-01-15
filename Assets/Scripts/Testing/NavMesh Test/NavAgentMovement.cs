65bfb99e2b6649747809c6e2ba8e26c57222985c
Evan Landry <Snoopy20015@gmail.com>
2020-01-14 17:13:59 -0800

    - Begun NavMesh production + Added personal scene
    Play testing with basic navmeshes. Starting production on Random pathing

---
+++ Assets/Scripts/Testing/NavMesh Test/NavAgentMovement.cs
@@ -0,0 +1,15 @@
+﻿// This is a test script to get the Agent to move along the mesh path
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentMovement : MonoBehaviour
+{
+    public Transform goal;
+
+    private void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
