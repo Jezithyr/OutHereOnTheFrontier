a33f6fa846c6fb4aabcc3e14d5fd97fc63f7e193
Evan Landry <evan.landryna@gmail.com>
2020-01-15 02:13:34 -0800

    - Added a NavMesh to the gym, added to the current PawnAI script.
    PawnAI script incomplete, pawn DOES NOT go to the desired destination. Perhaps we are missing something

--- Assets/Scripts/Testing/PawnAI/PawnMovementTest.cs
+++ Assets/Scripts/Testing/PawnAI/PawnMovementTest.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class PawnMovementTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    Transform destination;
+    NavMeshAgent navMeshAgent;
 
-    // Update is called once per frame
-    void Update()
+    void start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        if(navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent is not attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
     }
-}
+    private void SetDestination()
+    {
+        if (destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
+    }
+} 
