d62fa0b7b3ffb891cc073d9d464ff653f2f8dfe8
Evan Landry <evan.landryna@gmail.com>
2020-01-15 03:11:52 -0800

    - Added new NeshMap script for the AI
    Currently only moves to 1 waypoint. Too tired and sick to get it to the otherside, but it doesn't seem too complicated. Hopefully we can get this done tomorrow.

--- Assets/Scripts/Testing/PawnAI/PawnMovementTest.cs
+++ Assets/Scripts/Testing/PawnAI/PawnMovementTest.cs
@@ -1,31 +1,55 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class PawnMovementTest : MonoBehaviour
 {
     [SerializeField]
+    private Transform waypoint1; 
+    [SerializeField]
+    private Transform waypoint2;
+
     Transform destination;
+
     NavMeshAgent navMeshAgent;
+    private bool hasArrivedAtFirstWay = false;
 
-    void start()
+    void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        SetDestination();
 
-        if(navMeshAgent == null)
-        {
-            Debug.LogError("The nav mesh agent is not attached to " + gameObject.name);
-        }
-        else
-        {
-            SetDestination();
-        }
     }
+
     private void SetDestination()
     {
         if (destination != null)
         {
-            Vector3 targetVector = destination.transform.position;
-            navMeshAgent.SetDestination(targetVector);
+        Vector3 targetVector = destination.transform.position;
+        navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(waypoint1.position,gameObject.transform.position) < 5.0)
+        {
+            hasArrivedAtFirstWay = true;
+            if (hasArrivedAtFirstWay == true)
+            {
+            destination = waypoint2;
+            SetDestination();
+            }
+        }
+
+        if(Vector3.Distance(waypoint2.position,gameObject.transform.position) > 5.0)
+        {
+            hasArrivedAtFirstWay = false;
+            if (hasArrivedAtFirstWay == false)
+            {
+            destination = waypoint1;
+            SetDestination();
+            }
         }
     }
 } 
