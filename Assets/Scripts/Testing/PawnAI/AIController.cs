d4848f395d9b62a3ffb22a22e085ade79d8d52d4
Evan Landry <Snoopy20015@gmail.com>
2020-01-28 15:41:24 -0800

    + Added Patrol AI Script, Unit script, and Waypoint script
    - Scripts done in class with Willie, contains 1 error with a method call, will try to fix. 
    - If Cannot fix, may be able to refactor into current pawn movement script 
    - Alls well that ends well. Going home.

---
+++ Assets/Scripts/Testing/PawnAI/AIController.cs
@@ -0,0 +1,65 @@
+﻿// using System;
+// using System.Collections;
+// using UnityEngine;
+// using UnityEngine.AI;
+
+// [RequireComponent(typeof(NavMeshAgent))]
+
+// public class AIController : Unit
+// {
+    
+
+//     private NavMeshAgent Agent;
+//     private IEnumerator CurrentState;
+//     private Outpost TargetOutpost;
+
+//     protected override void UnitAwake()
+//     {
+//         Agent = GetComponent<NavMeshAgent>();
+//         RB.isKinematic = true;
+//     }
+
+//     private void Update()
+//     {
+//         Anim.SetFloat("Vertical", Agent.velocity.magnitude);  // Handle the animations.
+//     }
+
+//     private void SetState(IEnumerator newState)
+//     {
+//         if(CurrentState != null)
+//         {
+//             StopCoroutine(CurrentState);
+//         }
+
+//         CurrentState = newState;
+//         StartCoroutine(CurrentState);
+//     }
+
+//     private void OnEnable()
+//     {
+//         SetState(State_Idle());
+//     }
+
+//     private IEnumerator State_Idle()
+//     {
+//         while(TargetOutpost == null)
+//         {
+//             yield return null;
+//             TargetOutpost = Outpost.OutpostList.GetRandomItem();   // get a random outpost
+//         }
+//         SetState(State_MovingToOutpost());
+
+//     }
+
+//     private IEnumerator State_MovingToOutpost()
+//     {
+//         Agent.SetDestination(TargetOutpost.transform.position);
+//         while (Agent.remainingDistance > Agent.stoppingDistance)
+//         {
+//             yield return null;    
+//         }
+//         // Get a new random outpost
+//         TargetOutpost = null;
+//         SetState(State_Idle());
+//     }
+// }
---
+++ Assets/Scripts/Testing/PawnAI/Unit.cs
@@ -0,0 +1,23 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// [RequireComponent(typeof(Rigidbody))]  // This script MUST have a rigidbody to work. Do not need to NULL check
+// [RequireComponent(typeof(Animator))]  // This script MUST have a rigidbody to work. Do not need to NULL check
+// public abstract class Unit : MonoBehaviour
+// {
+//     [SerializeField] private Renderer Rend;
+
+//     protected Rigidbody RB;
+//     protected Animator Anim;
+    
+//     // The player will need to use the Physics components of the model. 
+//     private void Awake()
+//     {
+//         RB = GetComponent<Rigidbody>();
+//         Anim = GetComponent<Animator>();
+
+//         UnitAwake();
+//     }
+//     abstract protected void UnitAwake();
+// }
