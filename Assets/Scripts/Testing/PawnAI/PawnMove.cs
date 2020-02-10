a7c72bf784e26f3ba0e9864d817f70f9c3c0e485
Evan Landry <Snoopy20015@gmail.com>
2020-02-10 15:55:03 -0800

    - Replaced PawnAI movement system
    + Updated the movement system to only take 1 transform.

---
+++ Assets/Scripts/Testing/PawnAI/PawnMove.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PawnMove : MonoBehaviour
+{
+
+    [SerializeField] Transform destination;
+
+    NavMeshAgent navMeshAgent;
+
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        SetDestination();    
+    }
+
+    private void SetDestination()
+    {
+        if (destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
+    }
+}
