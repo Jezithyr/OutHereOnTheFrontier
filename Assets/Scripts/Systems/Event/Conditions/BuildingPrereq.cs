e3124cb19ecb423c1d0ef76371807c5e192206b0
Jesse Rougeau <Jmaster9999@gmail.com>
2020-02-11 18:41:33 -0800

    Added building Prereq for events

---
+++ Assets/Scripts/Systems/Event/Conditions/BuildingPrereq.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EventSystem/Conditions/Require Building")]
+public class BuildingPrereq : EventCondition
+{
+    [SerializeField] private Building buildingType;
+
+    public override bool ConditionCheck(ScriptableObject callingObject)
+    {
+        return buildingType.IsPlaced();
+    }
+}
